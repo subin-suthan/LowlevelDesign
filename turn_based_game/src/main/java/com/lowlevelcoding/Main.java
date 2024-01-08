ed51646e4a6fa922f287da24376f77842182b0d1
subin1998 <developtothrive@gmail.com>
2024-01-08 21:32:12 +0000

    Initialized repo

---
+++ turn_based_game/src/main/java/com/lowlevelcoding/Main.java
@@ -0,0 +1,33 @@
+package com.lowlevelcoding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public Board Start(){
+
+    }
+
+    public void play(Board board, Player player){
+
+    }
+
+    public GameResult isComplete(){
+
+    }
+}
+
+class Board{
+
+}
+
+
+class Player{
+
+}
+
+class GameResult{
+    
+}
